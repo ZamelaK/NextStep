b6b5cd44252d8c895a70dc8b94dedf7ef7b2f35f
Zamela Kwezela <zamelakwezela9@gmail.com>
2025-09-04 12:47:15 +0000

    can make the scroll button next to the dashboard more apealing and make

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,6 +1,7 @@
 
 import { DashboardHeader } from '@/components/layout/dashboard-header';
 import { DashboardSidebar } from '@/components/layout/dashboard-sidebar';
+import { ScrollArea } from '@/components/ui/scroll-area';
 import { SidebarProvider, SidebarInset } from '@/components/ui/sidebar';
 
 export default function DashboardLayout({
@@ -12,10 +13,14 @@ export default function DashboardLayout({
     <SidebarProvider>
       <div className="min-h-screen w-full flex">
         <DashboardSidebar />
-        <div className="flex-1 flex flex-col">
-          <DashboardHeader />
-          <main className="flex-1 p-4 md:p-6 lg:p-8 overflow-auto">{children}</main>
-        </div>
+        <SidebarInset>
+          <div className="flex-1 flex flex-col h-screen">
+            <DashboardHeader />
+            <ScrollArea className="flex-1">
+              <main className="p-4 md:p-6 lg:p-8">{children}</main>
+            </ScrollArea>
+          </div>
+        </SidebarInset>
       </div>
     </SidebarProvider>
   );
